4af74c7337e595fff91a7b12549ec4c986af0260
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-10-13 14:41:20 +0200

    ✨ App client contract.

---
+++ src/Contracts/App/AppClientContract.php
@@ -0,0 +1,59 @@
+<?php
+namespace Deegitalbe\TrustupProAdminCommon\Contracts\App;
+
+use stdClass;
+use Illuminate\Support\Collection;
+use Deegitalbe\TrustupProAdminCommon\Contracts\Models\AppContract;
+use Deegitalbe\TrustupProAdminCommon\Contracts\Models\AccountContract;
+
+/**
+ * API client able to communicate with application
+ * 
+ */
+interface AppClientContract
+{
+    /**
+     * Updating application account.
+     * 
+     * @param AccountContract $account
+     * @return bool success status.
+     */
+    public function updateAccount(AccountContract $account): ?stdClass;
+
+    /**
+     * Getting raw professional accounts.
+     * 
+     * @param Professional $professional
+     * @return Collection|null
+     */
+    public function getRawAccounts($professional): ?Collection;
+
+    /**
+     * Getting raw professional account matching given uuid.
+     * 
+     * @param Professional $professional
+     * @return stdClass|null
+     */
+    public function getRawAccount($professional, string $account_uuid): ?stdClass;
+
+    /**
+     * Getting all accounts.
+     * 
+     * @return Collection|null
+     */
+    public function getAllAccounts(): ?Collection;
+
+    /**
+     * Getting app linked to this client.
+     * 
+     * @return AppContract
+     */
+    public function getApp(): AppContract;
+
+    /**
+     * Creating client for given application.
+     * 
+     * @param AppContract $app
+     */
+    public static function forApp(AppContract $app);
+}

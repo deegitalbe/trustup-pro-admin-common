2eb8662fe209bb6768b738f8fb61b4bbb7a77fc6
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-10-13 14:48:39 +0200

    ✨ Registering publishable config + bind interfaces to implementations.

---
+++ src/Providers/TrustupProAdminCommonServiceProvider.php
@@ -0,0 +1,74 @@
+<?php
+namespace Deegitalbe\TrustupProAdminCommon\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Deegitalbe\TrustupProAdminCommon\Models\Account;
+use Deegitalbe\TrustupProAdminCommon\Facades\Package;
+use Deegitalbe\TrustupProAdminCommon\Models\AccountAccessEntry;
+use Deegitalbe\TrustupProAdminCommon\Contracts\Models\AppContract;
+use Deegitalbe\TrustupProAdminCommon\Package as UnderlyingPackage;
+use Deegitalbe\TrustupProAdminCommon\Models\AccountAccessEntryUser;
+use Deegitalbe\TrustupProAdminCommon\Contracts\Models\AccountContract;
+use Deegitalbe\TrustupProAdminCommon\Contracts\Models\AccountChargebeeContract;
+use Deegitalbe\TrustupProAdminCommon\Contracts\Models\AccountAccessEntryContract;
+use Deegitalbe\TrustupProAdminCommon\Contracts\Models\AccountAccessEntryUserContract;
+
+class TrustupProAdminCommonServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->registerConfig()
+            ->bindFacade()
+            ->bindModels();
+        
+        $this->app->bind(AppClientContract::class, AppClient::class);
+    }
+
+    public function boot()
+    {
+        $this->makeConfigPublishable();
+    }
+
+    protected function registerConfig(): self
+    {
+        $this->mergeConfigFrom($this->getConfigPath(), 'trustup-pro-admin-common');
+
+        return $this;
+    }
+
+    protected function bindFacade(): self
+    {
+        $this->app->bind('trustup_pro_admin_common', function($app) {
+            return new UnderlyingPackage();
+        });
+
+        return $this;
+    }
+
+    protected function bindModels(): self
+    {
+        $this->app->bind(AccountContract::class, Package::account());
+        $this->app->bind(AccountAccessEntryContract::class, Package::accountAccessEntry());
+        $this->app->bind(AccountAccessEntryUserContract::class, Package::accountAccessEntryUser());
+        $this->app->bind(AppContract::class, Package::app());
+        $this->app->bind(AccountChargebeeContract::class, Package::accountChargebee());
+
+        return $this;
+    }
+
+    protected function makeConfigPublishable(): self
+    {
+        if ($this->app->runningInConsole()):
+            $this->publishes([
+              $this->getConfigPath() => config_path('trustup-pro-admin-common.php'),
+            ], 'config');
+        endif;
+
+        return $this;
+    }
+
+    protected function getConfigPath(): string
+    {
+        return __DIR__.'/../config/config.php';
+    }
+}

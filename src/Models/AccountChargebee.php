d65853cf9e4848df3aafcc9227b1f003a232f7ab
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-10-15 14:39:12 +0200

    ✨ Scope limiting account chargebee to specific status.

--- src/Models/AccountChargebee.php
+++ src/Models/AccountChargebee.php
@@ -2,14 +2,47 @@
 namespace Deegitalbe\TrustupProAdminCommon\Models;
 
 use Carbon\Carbon;
-use Deegitalbe\TrustupProAdminCommon\Models\_Abstract\MongoModel;
+use Jenssegers\Mongodb\Eloquent\Builder;
+use Jenssegers\Mongodb\Relations\HasMany;
+use Illuminate\Database\Eloquent\Collection;
+use Deegitalbe\TrustupProAdminCommon\Facades\Package;
 use Deegitalbe\TrustupProAdminCommon\Contracts\Models\AccountContract;
+use Deegitalbe\TrustupProAdminCommon\Models\_Abstract\PersistableMongoModel;
 use Deegitalbe\TrustupProAdminCommon\Contracts\Models\AccountChargebeeContract;
 
-class AccountChargebee extends MongoModel implements AccountChargebeeContract
+class AccountChargebee extends PersistableMongoModel implements AccountChargebeeContract
 {
+    /**
+     * Status trial key.
+     * 
+     * @var string
+     */
+    const TRIAL = "in_trial";
+
+    /**
+     * Status active key.
+     * 
+     * @var string
+     */
+    const ACTIVE = "active";
+
+    /**
+     * Status cancel key.
+     * 
+     * @var string
+     */
+    const CANCELLED = "cancelled";
+
+    /**
+     * Status non renewing key.
+     * 
+     * @var string
+     */
+    const NON_RENEWING = "non_renewing";
+
     protected $fillable = [
-        'status'
+        'status',
+        'subscription_id'
     ];
 
     public function accounts(): HasMany
@@ -52,38 +85,94 @@ class AccountChargebee extends MongoModel implements AccountChargebeeContract
 
     public function getId(): string
     {
-        return $this->id;
+        return $this->subscription_id;
     }
 
     public function setId(string $id)
     {
-        $this->id = $id;
+        $this->subscription_id = $id;
 
         return $this;
     }
 
     public function isTrial(): bool
     {
-        return $this->status === "in_trial";
+        return $this->status === self::TRIAL;
     }
 
     public function isActive(): bool
     {
-        return $this->status === "active";
+        return $this->status === self::ACTIVE;
     }
 
     public function isCancelled(): bool
     {
-        return $this->status === "cancelled";
+        return $this->status === self::CANCELLED;
     }
 
     public function isNonRenewing(): bool
     {
-        return $this->status === "non_renewing";
+        return $this->status === self::NON_RENEWING;
     }
 
-    public function getAccount(): AccountContract
+    public function getAccounts(): Collection
     {
-        return $this->account;
+        return $this->accounts;
+    }
+
+    /**
+     * Limiting chargebee status to given status key.
+     * 
+     * @param Builder $query
+     * @param string $key
+     * @return Builder
+     */
+    public function scopeWhereStatus(Builder $query, string $key): Builder
+    {
+        return $query->where('status', $key);
+    }
+
+    /**
+     * Limiting chargebee status to in trial.
+     * 
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeInTrial(Builder $query): Builder
+    {
+        return $query->whereStatus(self::TRIAL);
+    }
+
+    /**
+     * Limiting chargebee status to active.
+     * 
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeActive(Builder $query): Builder
+    {
+        return $query->whereStatus(self::ACTIVE);
+    }
+
+    /**
+     * Limiting chargebee status to non renewing.
+     * 
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeNonRenewing(Builder $query): Builder
+    {
+        return $query->whereStatus(self::NON_RENEWING);
+    }
+
+    /**
+     * Limiting chargebee status to cancelled.
+     * 
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeCancelled(Builder $query): Builder
+    {
+        return $query->whereStatus(self::CANCELLED);
     }
 }

a166a373a05466f2c327f99b6144e41074a65ef9
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-10-25 10:55:05 +0200

    ♻ Package controller receiving version parameter to make sure package isnt outdated.

--- src/Http/Controllers/PackageController.php
+++ src/Http/Controllers/PackageController.php
@@ -1,17 +1,37 @@
 <?php
 namespace Deegitalbe\TrustupProAdminCommon\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Deegitalbe\TrustupProAdminCommon\Facades\Package;
+use Deegitalbe\TrustupProAdminCommon\Exceptions\Package\AdminPackageOutdated;
 
 class PackageController extends Controller
 {
     /**
-     * Getting project version.
+     * Checking package version.
      * 
      */
-    public function version()
+    public function version(Request $request)
     {
-        return response(['data' => Package::version()]);
+        $data = $request->validate(['version' => 'required|string']);
+        $version = $data['version'];
+        
+        // if version is outdated, write a log
+        if ($version > Package::version()):
+            report(
+                AdminPackageOutdated::getException()
+                    ->setNewVersion($version)
+            );
+
+            return response([
+                'data' =>[
+                    'is_outdated' => true,
+                    'new_version' => $version
+                ]
+            ]);
+        endif;
+
+        return response(['data' => ['is_outdated' => false]]);
     }
 }

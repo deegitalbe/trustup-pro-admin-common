1e260d0b8adfed82332c72be3c9cd2cdb419e9a0
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-12-14 15:28:14 +0100

    ✅ Testing new plan features.

--- tests/Unit/PlanTest.php
+++ tests/Unit/PlanTest.php
@@ -32,4 +32,48 @@ class PlanTest extends TestCase
         $this->assertEquals($name, App::find($app->getId())->getPlans()->first()->getName());
     }
 
+    /** @test */
+    public function plan_model_telling_if_having_trial_duration()
+    {
+        $plan = app()->make(PlanContract::class)
+            ->setName('test')
+            ->setTrialDuration(14);
+
+        $this->assertTrue($plan->hasTrialDuration());
+    }
+
+    /** @test */
+    public function plan_model_telling_if_not_having_trial_duration()
+    {
+        $plan = app()->make(PlanContract::class)
+            ->setName('test')
+            ->setTrialDuration(0);
+
+        $this->assertFalse($plan->hasTrialDuration());
+    }
+
+    /** @test */
+    public function plan_model_setting_price_in_cent()
+    {
+        $cent_price = 1000;
+        $plan = app()->make(PlanContract::class)
+            ->setName('test')
+            ->setPriceInCent($cent_price);
+
+        $this->assertEquals($cent_price, $plan->getPriceInCent());
+        $this->assertEquals($cent_price / 100, $plan->getPriceInEuro());
+    }
+
+    /** @test */
+    public function plan_model_setting_price_in_euro()
+    {
+        $euro_price = 10;
+        $plan = app()->make(PlanContract::class)
+            ->setName('test')
+            ->setPriceInEuro($euro_price);
+
+        $this->assertEquals($euro_price, $plan->getPriceInEuro());
+        $this->assertEquals($euro_price * 100, $plan->getPriceInCent());
+    }
+
 }

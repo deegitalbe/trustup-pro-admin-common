528d8b77d84ccac7f1b35ffd0f9a981deba8f746
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-12-14 15:34:58 +0100

    🚚 Removed test was moved to AppTest.

--- tests/Unit/PlanTest.php
+++ tests/Unit/PlanTest.php
@@ -8,30 +8,6 @@ use Deegitalbe\TrustupProAdminCommon\Contracts\Models\PlanContract;
 
 class PlanTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function plan_model_persistable()
-    {
-        $name = "test-plan";
-        $plan = app()->make(PlanContract::class)
-            ->setName($name)
-            ->setTrialDuration(14);
-
-        $app = app(AppContract::class)
-            ->setKey('agenda')
-            ->setUrl('https://agenda.trustup.pro')
-            ->setPaid(true)
-            ->setName('Un super agenda')
-            ->setDescription('une super description')
-            ->setAvailable(true)
-            ->setTranslated(true)
-            ->addPlan($plan)
-            ->persist();
-
-        $this->assertEquals($name, App::find($app->getId())->getPlans()->first()->getName());
-    }
-
     /** @test */
     public function plan_model_telling_if_having_trial_duration()
     {

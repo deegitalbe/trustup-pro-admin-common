bbaf802e2955f2c027932661fe24d467f28aa3db
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-07-07 15:38:42 +0200

    👔 Making sure trustup models are persistable.

--- src/Models/_Abstract/TrustupModel.php
+++ src/Models/_Abstract/TrustupModel.php
@@ -1,6 +1,7 @@
 <?php
 namespace Deegitalbe\TrustupProAdminCommon\Models\_Abstract;
 
+use Deegitalbe\TrustupProAdminCommon\Contracts\PersistableContract;
 use Deegitalbe\TrustupProAdminCommon\Models\Traits\BeingPersistable;
 use Deegitalbe\TrustupProAdminCommon\Models\Traits\Connections\TrustupConnection;
 use Deegitalbe\TrustupProAdminCommon\Models\Traits\CrossDatabaseRelations;
@@ -9,7 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * Extends this class to get a trustup database related model.
  */
-abstract class TrustupModel extends Model
+abstract class TrustupModel extends Model implements PersistableContract
 {
     use
         BeingPersistable,
